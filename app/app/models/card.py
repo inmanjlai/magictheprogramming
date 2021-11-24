a566d7489bbeae84070b560f5bfffef0c9b9e614
Ismail <inmanjlai@hotmail.com>
2021-11-24 15:44:51 -0500

    finished card model

---
+++ app/app/models/card.py
@@ -0,0 +1,24 @@
+from .db import db
+
+class Card(db.Model):
+    __tablename__ = 'cards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False, unique=True)
+    type_line = db.Column(db.String(80), nullable=False)
+    oracle_text = db.Column(db.String(4000), nullable=True)
+    mana_value = db.Column(db.Integer, nullable=False)
+    mana_cost = db.Column(db.String(40), nullable=True)
+    colors = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(6000), nullable=False)
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'type_line': self.type_line,
+            'oracle_text': self.oracle_text,
+            'mana_value': self.mana_value,
+            'mana_cost': self.mana_cost,
+            'colors': self.colors,
+            'image_url': self.image_url,
+        }
