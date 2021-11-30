b796046f21096255baae9c6f090ed2eabad6e527
baselHassan <baselhassan8@gmail.com>
2021-11-29 18:52:49 -0600

    background image for splash

--- app/react-app/src/App.js
+++ app/react-app/src/App.js
@@ -12,6 +12,7 @@ import Deck from './components/Deck';
 import SingleDeck from './components/Deck/SingleDeck';
 import CreateDeck from './components/Deck/CreateDeck';
 import EditDeck from './components/Deck/EditDeck';
+import Splash from './components/Splash/Splash';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -30,46 +31,53 @@ function App() {
  
   return (
     <BrowserRouter>
-      <NavBar />
       <Switch>
 
         <Route path='/login' exact={true}>
+          <NavBar />
           <LoginForm />
         </Route>
 
         <Route path='/sign-up' exact={true}>
+          <NavBar />
           <SignUpForm />
         </Route>
 
         <ProtectedRoute path='/users' exact={true} >
+          <NavBar />
           <UsersList/>
         </ProtectedRoute>
 
         <ProtectedRoute path='/users/:userId' exact={true} >
+          <NavBar />
           <User />
         </ProtectedRoute>
 
         <Route path='/' exact={true} >
-          <h1>My Home Page</h1>
+          <Splash />
         </Route>
 
         {/* CUSTOM ROUTES */}
         <Route path='/decks' exact={true}>
           <h2>See a list of decks</h2>
+          <NavBar />
           <Deck />
         </Route>
 
         <Route path='/decks/:deckId' exact={true}>
+          <NavBar />
           <SingleDeck />
         </Route>
 
         <ProtectedRoute path='/new-deck' exact={true} >
           <h1>Create a deck</h1>
+          <NavBar />
           <CreateDeck />
         </ProtectedRoute>
 
         <ProtectedRoute path='/decks/:deckId/edit' exact={true} >
           <h1>Edit deck</h1>
+          <NavBar />
           <EditDeck />
         </ProtectedRoute>
 
---
+++ app/react-app/src/components/Splash/Splash.js
@@ -0,0 +1,22 @@
+import NavBar from "../Navbar/NavBar"
+import './Splash.css'
+
+const Splash = () => {
+    return (
+        <>
+            <div className='hero-bg'>
+                <img src='https://www.moxfield.com/img/hero-bg.jpg' alt="" />
+            </div>
+            <div className="navContainer">
+                <NavBar />
+            </div>
+            <div className="mainContainer">
+
+            </div>
+            <div className="footer"></div>
+        </>
+        
+    )
+}
+
+export default Splash
